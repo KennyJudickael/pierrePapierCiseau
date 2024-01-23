800967f093d44affa794d53154396833bff082c4
RAVOTINA Kenny Judickael <judickaelkenny@gmail.com>
2024-01-23 12:31:58 +0300

    final commit

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
 const resultDisplay = document.getElementById("resultDisplay");
 
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+let playerScore = 0;
+let computerScore = 0;
+
 function playGame(playChoice) {
   const computerChoise = choices[Math.floor(Math.random() * 3)];
 
@@ -34,9 +39,13 @@ function playGame(playChoice) {
   switch (result) {
     case "YOU WIN":
       resultDisplay.classList.add("greenText");
+      playerScore++;
+      playerScoreDisplay.textContent = playerScore;
       break;
     case "YOU LOSE":
       resultDisplay.classList.add("redText");
+      computerScore++;
+      computerScoreDisplay.textContent = computerScore;
       break;
   }
 }
